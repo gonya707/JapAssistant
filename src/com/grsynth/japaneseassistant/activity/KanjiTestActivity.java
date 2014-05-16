5af3ac135a0c8524b460e2dd8664d8de167360ea
Gonzalo Recio <gonya707@gmail.com>
2014-05-16 19:40:28 +0200

    test settings functional. test mockup screen
    bits and pieces from some other files. The test settings crash under
    some conditions, gotta check later. (e.g. selecting levels 5 & 1)

--- src/com/grsynth/japaneseassistant/activity/KanjiTestActivity.java
+++ src/com/grsynth/japaneseassistant/activity/KanjiTestActivity.java
@@ -1,16 +1,33 @@
 package com.grsynth.japaneseassistant.activity;
 
-import com.grsynth.japaneseassistant.R;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.grsynth.japaneseassistant.R;
+import com.grsynth.japaneseassistant.Type.Kanji;
 
 public class KanjiTestActivity extends Activity {
 
 	@Override
+	@SuppressWarnings("unchecked")
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_kanji_test);
+		
+		TextView tv = (TextView) findViewById(R.id.tv);
+
+		int nQuestion = getIntent().getExtras().getInt("nQuestion");
+		int category = getIntent().getExtras().getInt("category");
+		ArrayList<Kanji> kanjiList = (ArrayList<Kanji>) getIntent().getExtras().getSerializable("list");
+
+		tv.append("\n" + kanjiList.get(0).getKanji());
+		tv.append("\n" + kanjiList.get(1).getKanji());
+		tv.append("\n" + kanjiList.get(2).getKanji());
+		tv.append("\n" + nQuestion);
+		tv.append("\n" + category);
 	}
 
 }
--- src/com/grsynth/japaneseassistant/activity/KanjiTestSettingsActivity.java
+++ src/com/grsynth/japaneseassistant/activity/KanjiTestSettingsActivity.java
@@ -1,5 +1,9 @@
 package com.grsynth.japaneseassistant.activity;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +13,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.grsynth.japaneseassistant.R;
+import com.grsynth.japaneseassistant.Type.Kanji;
 
 public class KanjiTestSettingsActivity extends Activity{
 
@@ -26,23 +33,65 @@ public class KanjiTestSettingsActivity extends Activity{
 		final CheckBox chkIos3 = (CheckBox) findViewById(R.id.checkBox3);
 		final CheckBox chkIos4 = (CheckBox) findViewById(R.id.checkBox4);
 		final CheckBox chkIos5 = (CheckBox) findViewById(R.id.checkBox5);
+		final Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
 		btnDisplay.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 
-				RadioButton rb;
-				rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
-//
-//				if (chkIos1.isChecked()) {
-//
-//				}
-
-				//rb.getText()
+				Kanji k;
+				ArrayList<Kanji> kanjiList= new ArrayList<Kanji>();
+				RadioButton rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
 
 				if (chkIos1.isChecked() || chkIos2.isChecked() || chkIos3.isChecked() || chkIos4.isChecked() || chkIos5.isChecked()){
+
+					//List
+					try{
+						FileInputStream fin = openFileInput("kanji");
+						ObjectInputStream ois = new ObjectInputStream(fin);
+						for(int i = 0; i < getResources().getInteger(R.integer.number_of_kanji); i++){
+							k = (Kanji) ois.readObject();
+							if (chkIos1.isChecked() && k.getIntJlpt() == 1 ||
+									chkIos2.isChecked() && k.getIntJlpt() == 2 ||
+									chkIos3.isChecked() && k.getIntJlpt() == 3 ||
+									chkIos4.isChecked() && k.getIntJlpt() == 4 ||
+									chkIos5.isChecked() && k.getIntJlpt() == 5){
+								kanjiList.add(k);
+							}
+						}
+						ois.close();
+					}catch(Exception ex){
+						ex.printStackTrace();
+					} 
+
+					int questions = 0;
+					//N of questions
+					switch(spinner.getSelectedItemPosition()){
+					case 0:
+						questions = 10;
+						break;
+					case 1:
+						questions = 25;
+						break;
+					case 2:
+						questions = 50;
+						break;
+					case 3:
+						questions = 100;
+						break;
+					case 4:
+						questions = 0;
+						break;
+					}
+
 					Intent toAnotherActivity = new Intent(v.getContext(), KanjiTestActivity.class);
+					toAnotherActivity.putExtra("nQuestion", questions);
+					toAnotherActivity.putExtra("category", rg.indexOfChild(rb)); //TODO
+					toAnotherActivity.putExtra("list", kanjiList);
 					startActivityForResult(toAnotherActivity, 0);
+				}
+				else{
+					Toast.makeText(getApplicationContext(), "Please select at least one level", Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
